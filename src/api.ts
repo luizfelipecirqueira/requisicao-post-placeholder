60d0b0c2e3fb866fc1637e3411ec4329e83f0483
luizfelipecirqueira <lfcs18ts@gmail.com>
2022-07-01 05:16:54 -0300

    Utilizando axiosInstance

--- src/api.ts
+++ src/api.ts
@@ -1,21 +1,27 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+})
 
 export const api = {
 
 
     getAllPosts: async () => {
-        let response = await axios.get(`${BASE_URL}/posts`);
+        //let response = await axios.get(`${BASE_URL}/posts`);
+        let response = await axiosInstance.get('/posts');
         return response.data;
         /*let response = await fetch(`${BASE_URL}/posts`);
         let json = await response.json();
         return json;*/
     },
     addNewPost: async (title: string, body: string, userId: number) => {
-        let response = await axios.post(`${BASE_URL}/posts`, {
+        let response = await axiosInstance.post('/posts', {
             title, body, userId
         });
+        /*let response = await axios.post(`${BASE_URL}/posts`, {
+            title, body, userId
+        });*/
         return response.data;
         /*let response = await fetch(`${BASE_URL}/posts`, {
             method: 'POST',
